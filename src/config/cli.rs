41d0dab03dca3e2f969c3fda0fbc57f37b2adb14
shikai liu <lsk569937453@gmail.com>
2025-12-23 09:19:01 +0800

    Refactor: make CLI command name and description configurable

--- src/config/cli.rs
+++ src/config/cli.rs
@@ -2,8 +2,8 @@ use clap::Parser;
 
 /// SGS 后端服务器
 #[derive(Debug, Parser)]
-#[command(name = "sgs-backend")]
-#[command(about = "SGS Game Backend Server")]
+#[command(name = "{{project_name}}")]
+#[command(about = "Backend Server")]
 #[command(version)]
 pub struct Cli {
     /// 配置文件路径
